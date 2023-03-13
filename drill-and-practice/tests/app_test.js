ce992c0aaaceac8b6699bbea5ea671f85a9d2723
AndrewTrieu <andrewtrieu.2003@gmail.com>
2023-03-13 12:52:06 +0200

    Change app_test.js

--- drill-and-practice/tests/app_test.js
+++ drill-and-practice/tests/app_test.js
@@ -1,12 +1,1 @@
-import { app } from "../app.js";
-import { superoak } from "https://deno.land/x/superoak@4.4.0/mod.ts";
-
-Deno.test("Home page works", async () => {
-  const testClient = await superoak(app);
-  const request = testClient
-    .get("https://quizzy.fly.dev")
-    .set("Origin", "http://localhost:8000");
-  await request
-    .expect(new RegExp("Quizzy"))
-    .expect("Access-Control-Allow-Origin", "http://localhost:8000");
-});
+// Tested using Playwright. Please go to e2e-playwright in the parent folder. This file is left empty on purpose.
